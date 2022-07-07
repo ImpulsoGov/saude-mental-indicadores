bad6df67a54ecb12ea625b38cc46e0155405b6d6
bcbernardo <bernardo@impulsogov.org>
2022-07-07 19:51:51 -0300

    perf(referencias): :zap: Implementa atualizações incrementais

--- models/referencias_ambulatoriais/atendimentos.sql
+++ models/referencias_ambulatoriais/atendimentos.sql
@@ -7,24 +7,33 @@ SPDX-License-Identifier: MIT
 
 WITH
 bpa_i AS (
-    SELECT * FROM {{ source('siasus', 'bpa_i_disseminacao') }}
+    SELECT *
+    FROM {{ source('siasus', 'bpa_i_disseminacao') }}
+    {% if is_incremental() -%}
+    WHERE atualizacao_data > (
+        SELECT max(atualizacao_data) FROM {{ this }}
+    )
+    {%- endif %}
 ),
 estabelecimentos_referencia AS (
     SELECT * FROM {{ ref('estabelecimentos_referencia_ambulatorial') }}
-)
-SELECT
-    bpa_i.*,
-    {{ classificar_faixa_etaria(
-        "bpa_i.usuario_data_nascimento",
-        "bpa_i.realizacao_periodo_data_inicio",
-        [10, 20, 30, 40, 50, 60]
-    ) }} AS usuario_faixa_etaria
-FROM bpa_i
-INNER JOIN estabelecimentos_referencia
-ON
-    bpa_i.estabelecimento_id_cnes
-    = estabelecimentos_referencia.estabelecimento_id_cnes
-WHERE bpa_i.profissional_ocupacao_id_cbo IN (
-    '251510',  -- psicólogos
-    '225133'  -- psiquiatras
-)
+),
+bpa_i_referencias_ambulatoriais AS (
+    SELECT
+        {{ limpar_datas_atualizacao(source('siasus', 'bpa_i_disseminacao')) }},
+        now() AS atualizacao_data
+    FROM bpa_i
+    INNER JOIN estabelecimentos_referencia
+    USING (estabelecimento_id_cnes)
+    WHERE bpa_i.profissional_ocupacao_id_cbo IN (
+        '251510',  -- psicólogos
+        '225133'  -- psiquiatras
+    )
+),
+{{ classificar_faixa_etaria(
+    relacao="bpa_i_referencias_ambulatoriais",
+    coluna_data_nascimento="usuario_data_nascimento",
+    coluna_data_referencia="realizacao_periodo_data_inicio",
+    cte_resultado="final"
+)}}
+SELECT * FROM final
