4d26128c1d1410bf70eb05260dc96c55191e030c
bcbernardo <bernardo@impulsogov.org>
2022-08-04 18:52:37 -0300

    feat(caps): :sparkles: Adiciona modelo com último envio de RAAS por município.

---
+++ models/ultimas_competencias_disponiveis/raas_psicossocial_disseminacao_ultima_competencia.sql
@@ -0,0 +1,25 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+raas_psicossocial_disseminacao AS (
+    SELECT * FROM {{ source("siasus", "raas_psicossocial_disseminacao") }}
+),
+final AS (
+    SELECT
+        DISTINCT ON (
+            unidade_geografica_id
+        )
+        unidade_geografica_id,
+        periodo_id,
+        processamento_periodo_data_inicio AS periodo_data_inicio
+    FROM raas_psicossocial_disseminacao
+    ORDER BY
+        unidade_geografica_id,
+        processamento_periodo_data_inicio DESC
+)
+SELECT * FROM final
