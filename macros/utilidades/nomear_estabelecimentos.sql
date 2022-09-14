ae6655837e86de2e3b564caa6dcd827ddd463556
bcbernardo <bernardo@impulsogov.org>
2022-09-14 17:41:21 -0300

    refactor: :recycle: Altera macros para admitir diferentes nomes de colunas com categorias
    Permite que qualquer coluna com códigos das categorias `sexo`, `raca_cor` e `estabelecimento` e um sufixo `_id_*` seja automaticamente substituída pelo nome legível da categoria.

--- macros/utilidades/nomear_estabelecimentos.sql
+++ macros/utilidades/nomear_estabelecimentos.sql
@@ -13,6 +13,15 @@ SPDX-License-Identifier: MIT
     todos_estabelecimentos_valor="Todos",
     cte_resultado="com_nomes_estabelecimentos"
 ) %}
+{%- set re = modules.re -%}
+{%- set lista_de_codigos_colunas = adapter.get_columns_in_relation(
+    source("codigos", "estabelecimentos")
+) -%}
+{%- set lista_de_codigos_coluna_id = "estabelecimento" + re.match(
+        ".*estabelecimento.*(_id.*)",
+        coluna_estabelecimento_id
+    ).groups(1)[0]
+ -%}
 {{ cte_resultado }} AS (
 SELECT
     t.*,
@@ -20,7 +29,8 @@ SELECT
     (
         CASE
             WHEN
-            {{ coluna_estabelecimento_id }} = '{{ todos_estabelecimentos_id }}'
+                t.{{ coluna_estabelecimento_id }}
+                = '{{ todos_estabelecimentos_id }}'
             THEN '{{ todos_estabelecimentos_valor }}'
         ELSE
 {%- endif %}    coalesce(
@@ -29,18 +39,18 @@ SELECT
             )
     {%- if todos_sexos_id is not none -%}
         END
-    ){%- endif %} AS {{ coluna_estabelecimento_nome}}
+    ){%- endif %} AS {{ coluna_estabelecimento_nome }}
 FROM {{ relacao }} t
 LEFT JOIN (
     SELECT 
-    {%- for coluna in adapter.get_columns_in_relation(
-        source("codigos", "estabelecimentos")
-    ) %}
+    {%- for coluna in lista_de_codigos_colunas %}
         {{coluna.quoted}} AS "estabelecimento_{{coluna.name}}"
         {{- "," if not loop.last }}
     {%- endfor %}
     FROM {{ source("codigos", "estabelecimentos") }}
 ) estabelecimento
-USING ({{ coluna_estabelecimento_id }})
+ON 
+    t.{{ coluna_estabelecimento_id }}
+    = estabelecimento.{{ lista_de_codigos_coluna_id }}
 )
 {%- endmacro -%}
