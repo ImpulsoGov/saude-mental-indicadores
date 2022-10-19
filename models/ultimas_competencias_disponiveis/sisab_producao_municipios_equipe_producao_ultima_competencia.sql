b53ce9195cc6e8a6c0ecf6c39615a4971a1d1ef5
fernandesbruna <brunadesfernandes@gmail.com>
2022-10-19 19:04:00 -0300

    Corrige modelo para ultima competencia do Sisab

--- models/ultimas_competencias_disponiveis/sisab_producao_municipios_equipe_producao_ultima_competencia.sql
+++ models/ultimas_competencias_disponiveis/sisab_producao_municipios_equipe_producao_ultima_competencia.sql
@@ -8,37 +8,32 @@ SPDX-License-Identifier: MIT
 WITH
 sisab_producao_municipios_por_tipo_equipe_por_tipo_producao AS (
     SELECT * FROM {{ source('sisab', 'sisab_producao_municipios_por_tipo_equipe_por_tipo_producao') }}
-        WHERE
-        tipo_equipe = 'Eq. Consultório na Rua - ECR'
-    AND tipo_producao = 'Atendimento Individual'
 ),
 periodos AS (
     SELECT * FROM {{ source('codigos', 'periodos') }}
 ),
-unidades_geograficas AS (
-    SELECT * FROM {{ source('codigos', 'unidades_geograficas') }}
-),
 preparacao AS (
-    SELECT
-        unidade_geografica_id,
-        periodo_id
-    FROM sisab_producao_municipios_por_tipo_equipe_por_tipo_producao
-    GROUP BY
-        unidade_geografica_id,
-        periodo_id
-        ),
-final AS (
-    SELECT
-        preparacao.unidade_geografica_id,
+    SELECT 
+        sisab_producao_municipios_por_tipo_equipe_por_tipo_producao.unidade_geografica_id,
+        sisab_producao_municipios_por_tipo_equipe_por_tipo_producao.periodo_id,
         periodos.data_inicio AS periodo_data_inicio,
-        periodos.id AS periodo_id
-
-    FROM preparacao
+        periodos.id
+    FROM sisab_producao_municipios_por_tipo_equipe_por_tipo_producao
     LEFT JOIN periodos
     ON 
-        preparacao.periodo_id = periodos.id
+        sisab_producao_municipios_por_tipo_equipe_por_tipo_producao.periodo_id = periodos.id
     AND periodos.tipo = 'Mensal'
-    LEFT JOIN listas_de_codigos.unidades_geograficas unidade_geografica
-    ON preparacao.unidade_geografica_id = unidade_geografica.id
+),
+final AS (
+    SELECT DISTINCT ON (
+            unidade_geografica_id
+        )
+        unidade_geografica_id,
+        periodo_id,
+        periodo_data_inicio
+    FROM preparacao
+    ORDER BY
+        unidade_geografica_id,
+        periodo_data_inicio DESC
 )
 SELECT * FROM final
