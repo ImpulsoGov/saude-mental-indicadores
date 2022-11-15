4378a503f2367f0a8a1f7569570b1a89a2a96478
bcbernardo <bernardo@impulsogov.org>
2022-11-15 17:40:10 -0300

    feat(internacoes): :sparkles: Adiciona macro e seed p/ classificar duração das internações

---
+++ macros/classificar_categorias/classificar_internacao_duracao.sql
@@ -0,0 +1,36 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+{%- macro 
+    classificar_internacao_duracao(
+        relacao,
+        coluna_entrada_data,
+        coluna_desfecho_data,
+        colunas_internacao_duracao=["id", "descricao", "ordem"],
+        cte_resultado="com_duracao_internacao"
+    )
+-%}
+{%- set duracao -%}
+AGE(t.{{ coluna_desfecho_data }}, t.{{ coluna_entrada_data }})
+{%- endset -%}
+duracoes_internacoes AS (
+    SELECT * FROM {{ ref("internacoes_duracoes") }}
+),
+{{ cte_resultado }} AS (
+    SELECT 
+        t.*,
+        {% for coluna in colunas_duracao_internacao -%}
+        duracao_internacao.{{- coluna }} AS duracao_internacao_{{- coluna -}}
+        {{- "," if not loop.last }}
+        {%- endfor %}
+    FROM {{ relacao }} t
+    LEFT JOIN duracoes_internacoes duracao_internacao
+    ON
+        {{ duracao }} >= duracao_internacao.minimo
+    AND {{ duracao }} < duracao_internacao.maximo
+)
+{%- endmacro -%}
