d22c4e7b7ecd5bcc0c7082ea5d19db296c3aafcb
fernandesbruna <brunadesfernandes@gmail.com>
2022-12-22 19:09:47 -0300

    feat: :sparkles: Adiciona modelo de última competência para procedimentos SIASUS
    Adiciona modelo de última competência para procedimentos SIASUS

---
+++ models/ultimas_competencias_disponiveis/procedimentos_disseminacao_ultima_competencia.sql
@@ -0,0 +1,25 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+procedimentos_disseminacao AS (
+    SELECT * FROM {{ source('siasus', 'procedimentos_disseminacao') }}
+),
+final AS (
+    SELECT
+        DISTINCT ON (
+            unidade_geografica_id
+        )
+        unidade_geografica_id,
+        periodo_id,
+        realizacao_periodo_data_inicio AS periodo_data_inicio
+    FROM procedimentos_disseminacao
+    ORDER BY
+        unidade_geografica_id,
+        realizacao_periodo_data_inicio DESC
+)
+SELECT * FROM final
