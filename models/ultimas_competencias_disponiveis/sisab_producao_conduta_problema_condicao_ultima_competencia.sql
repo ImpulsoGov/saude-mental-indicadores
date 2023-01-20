e4f602ca47ee05af5e5f96480400477f2ae81765
fernandesbruna <brunadesfernandes@gmail.com>
2023-01-20 09:48:39 -0300

    feat: :sparkles: Adiciona modelo de última competência para a base do SISAB por conduta, problema e condição avaliada
    Adiciona modelo de última competência para a base do SISAB por conduta, problema e condição avaliada

---
+++ models/ultimas_competencias_disponiveis/sisab_producao_conduta_problema_condicao_ultima_competencia.sql
@@ -0,0 +1,39 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+sisab_producao_conduta_problema_condicao AS (
+    SELECT * FROM {{ source('sisab', 'sisab_producao_conduta_problema_condicao') }}
+),
+periodos AS (
+    SELECT * FROM {{ source('codigos', 'periodos') }}
+),
+preparacao AS (
+    SELECT 
+        sisab_producao_conduta_problema_condicao.unidade_geografica_id,
+        sisab_producao_conduta_problema_condicao.periodo_id,
+        periodos.data_inicio AS periodo_data_inicio,
+        periodos.id
+    FROM sisab_producao_conduta_problema_condicao
+    LEFT JOIN periodos
+    ON 
+        sisab_producao_conduta_problema_condicao.periodo_id = periodos.id
+    AND periodos.tipo = 'Mensal'
+),
+final AS (
+    SELECT DISTINCT ON (
+            unidade_geografica_id
+        )
+        unidade_geografica_id,
+        periodo_id,
+        periodo_data_inicio
+    FROM preparacao
+    ORDER BY
+        unidade_geografica_id,
+        periodo_data_inicio DESC
+)
+SELECT * FROM final
