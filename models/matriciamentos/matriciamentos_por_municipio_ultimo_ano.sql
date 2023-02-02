a6bfb36735224d8b3f847604f43909a7a490e82a
Bruna Fernandes <108029366+fernandesbruna@users.noreply.github.com>
2023-02-02 19:54:29 -0300

    feat: :sparkles: Adiciona modelos de matriciamentos (#23)
    Adiciona modelos de matriciamentos por CAPS e por municípios para o ano da última competência disponível

---
+++ models/matriciamentos/matriciamentos_por_municipio_ultimo_ano.sql
@@ -0,0 +1,30 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+matriciamentos_por_caps AS (
+    SELECT * FROM {{ ref('matriciamentos_por_caps_ultimo_ano') }}
+),
+matriciamentos_por_municipio AS (
+    SELECT
+        unidade_geografica_id,
+        unidade_geografica_id_sus,
+        date_part('year', max(competencia))::text AS ano,
+        listas_de_codigos.nome_mes(max(competencia)) AS ate_mes,
+        sum(quantidade_registrada) AS quantidade_registrada,
+        count(DISTINCT estabelecimento) FILTER (
+            WHERE media_mensal_para_meta <= 1
+        ) AS estabelecimentos_na_meta,
+        count(DISTINCT estabelecimento) FILTER (
+            WHERE media_mensal_para_meta > 1
+        ) AS estabelecimentos_fora_meta
+    FROM matriciamentos_por_caps
+    GROUP BY
+        unidade_geografica_id,
+        unidade_geografica_id_sus
+)
+SELECT * FROM matriciamentos_por_municipio
