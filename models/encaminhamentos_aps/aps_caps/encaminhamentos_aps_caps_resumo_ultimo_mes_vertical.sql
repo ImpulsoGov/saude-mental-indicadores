692502b015e404543aec2d51d61c0b7220859fd1
Bruna Fernandes <108029366+fernandesbruna@users.noreply.github.com>
2023-03-23 11:58:29 -0300

    feat: :sparkles: Adiciona modelos verticais de encaminhamentos para telas resumo (#27)
    Adiciona modelos verticais de encaminhamentos para telas resumo

---
+++ models/encaminhamentos_aps/aps_caps/encaminhamentos_aps_caps_resumo_ultimo_mes_vertical.sql
@@ -0,0 +1,43 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+ 
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+encaminhamentos_aps_caps_horizontal AS (
+    SELECT * FROM {{ ref('encaminhamentos_aps_caps_resumo_ultimo_mes_horizontal') }}
+),
+view_vertical AS (
+    SELECT
+        unidade_geografica_id,
+        unidade_geografica_id_sus,
+        periodo_id,
+        competencia,
+        nome_mes,
+        'Sim' AS encaminhamento,
+        (perc_encaminhamentos_caps) / 100 AS prop_atendimentos
+    FROM encaminhamentos_aps_caps_horizontal
+    UNION
+    SELECT
+        unidade_geografica_id,
+        unidade_geografica_id_sus,
+        periodo_id,
+        competencia,
+        nome_mes,
+        'Não' AS encaminhado,
+        (100 - perc_encaminhamentos_caps) / 100 AS prop_atendimentos
+    FROM encaminhamentos_aps_caps_horizontal
+),
+final AS (
+    SELECT
+    {{ dbt_utils.surrogate_key([
+        "unidade_geografica_id",
+        "periodo_id",
+        "encaminhamento"
+    ]) }} AS id,
+     *
+    FROM view_vertical
+)
+SELECT * FROM final
