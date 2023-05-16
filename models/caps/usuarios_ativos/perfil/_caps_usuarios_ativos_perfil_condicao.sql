5cadcb4f5c8ead67225442b422f83e95ddcb4bb8
fernandesbruna <brunadesfernandes@gmail.com>
2023-05-16 17:21:35 -0300

    feat: :construction: [WIP] Adiciona modelos de perfil quebrados por gráfico para usuários ativos
    [WIP] Adiciona modelos de perfil quebrados por gráfico para usuários ativos

---
+++ models/caps/usuarios_ativos/perfil/_caps_usuarios_ativos_perfil_condicao.sql
@@ -0,0 +1,103 @@
+{#
+SPDX-FileCopyrightText: 2022 ImpulsoGov <contato@impulsogov.org>
+
+SPDX-License-Identifier: MIT
+#}
+
+
+WITH
+usuarios_ativos_perfil AS (
+    SELECT * FROM {{ ref("_caps_usuarios_ativos_perfil") }}
+),
+
+-- Calcula subtotais para todos os estabelecimentos
+{{ calcular_subtotais(
+    relacao="usuarios_ativos_perfil",
+    agrupar_por=[
+        "unidade_geografica_id",
+        "unidade_geografica_id_sus",
+        "periodo_id",
+        "periodo_data_inicio",
+        "usuario_situacao_rua",
+        "usuario_abuso_substancias"
+    ],
+    colunas_a_totalizar=[
+		"estabelecimento_linha_perfil",
+    	"estabelecimento_linha_idade",
+		"estabelecimento_id_scnes"
+    ],
+    nomes_categorias_com_totais=["Todos", "Todos", "0000000"],
+    agregacoes_valores={
+        "ativos_mes": "sum",
+        "ativos_3meses": "sum",
+        "tornandose_inativos": "sum",
+        "atualizacao_data": "max"
+    },
+    manter_original=true,
+    cte_resultado="usuario_perfil_incluindo_totais"
+) }},
+-- Separa apenas o estabelecimento Todos (0000000)
+usuario_perfil_incluindo_apenas_totais AS (
+    SELECT *
+    FROM usuario_perfil_incluindo_totais
+    WHERE estabelecimento_id_scnes = '0000000'
+),
+-- separa tudo que não é todos e tira as totalizações por linha e idade. Não seria um passo necessário utilizando a CTE 'por_estabelecimento_com_linhas_cuidado', mas foi preciso porque com essa opção ocorria um erro com os tipos das colunas que são UUID
+usuario_perfil_excluindo_totais AS (
+    SELECT *
+    FROM usuario_perfil_incluindo_totais
+    WHERE estabelecimento_id_scnes <> '0000000' AND estabelecimento_linha_idade <> 'Todos' AND estabelecimento_linha_perfil <> 'Todos'
+),
+-- Junta com demais estabelecimentos sem o cálculo de subtotais por linha e idade de cuidado
+usuario_perfil_totais_e_individuais AS (
+    SELECT * FROM usuario_perfil_incluindo_apenas_totais
+    UNION
+    SELECT * FROM usuario_perfil_excluindo_totais
+),
+{{  revelar_combinacoes_implicitas(
+    relacao="usuario_perfil_totais_e_individuais",
+    agrupar_por=[
+        "unidade_geografica_id",
+        "unidade_geografica_id_sus",
+    ],
+    colunas_a_completar=[
+        ["periodo_id", "periodo_data_inicio"],
+        ["estabelecimento_id_scnes"],
+        ["estabelecimento_linha_perfil"],
+    	["estabelecimento_linha_idade"],
+        ["usuario_situacao_rua"],
+        ["usuario_abuso_substancias"]
+    ],
+    cte_resultado="com_combinacoes"
+) }},
+final AS (
+    SELECT
+        {{ dbt_utils.surrogate_key([
+            "unidade_geografica_id",
+		    "estabelecimento_linha_perfil",
+    	    "estabelecimento_linha_idade",
+            "estabelecimento_id_scnes",
+            "periodo_id",
+            "usuario_situacao_rua",
+            "usuario_abuso_substancias"
+        ]) }} AS id,
+        unidade_geografica_id,
+        unidade_geografica_id_sus,
+        periodo_id,
+        periodo_data_inicio,
+		estabelecimento_linha_perfil,
+		estabelecimento_linha_idade,
+        estabelecimento_id_scnes,
+        saude_mental.classificar_binarios(
+            usuario_situacao_rua
+        ) AS usuario_situacao_rua,
+        saude_mental.classificar_binarios(
+            usuario_abuso_substancias
+        ) AS usuario_abuso_substancias,
+        coalesce(ativos_mes, 0) AS ativos_mes,
+        coalesce(ativos_3meses, 0) AS ativo_3meses,
+        coalesce(tornandose_inativos, 0) AS tornandose_inativos,
+        now() AS atualizacao_data
+    FROM com_combinacoes 
+)
+SELECT * FROM final
