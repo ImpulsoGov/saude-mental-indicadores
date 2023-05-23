6ed9d11c9d0600624ab6a84ff6a2df70d3622d0f
fernandesbruna <brunadesfernandes@gmail.com>
2023-05-23 13:19:35 -0300

    style: :pencil2: Corrige typo em nome de colunas

--- models/caps/usuarios_ativos/perfil/_caps_usuarios_ativos_perfil_condicao.sql
+++ models/caps/usuarios_ativos/perfil/_caps_usuarios_ativos_perfil_condicao.sql
@@ -95,7 +95,7 @@ final AS (
             usuario_abuso_substancias
         ) AS usuario_abuso_substancias,
         sum(coalesce(ativos_mes, 0)) AS ativos_mes,
-        sum(coalesce(ativos_3meses, 0)) AS ativo_3meses,
+        sum(coalesce(ativos_3meses, 0)) AS ativos_3meses,
         sum(coalesce(tornandose_inativos, 0)) AS tornandose_inativos,
         now() AS atualizacao_data
     FROM com_combinacoes 
--- models/caps/usuarios_ativos/perfil/_caps_usuarios_ativos_perfil_genero_idade.sql
+++ models/caps/usuarios_ativos/perfil/_caps_usuarios_ativos_perfil_genero_idade.sql
@@ -96,7 +96,7 @@ final AS (
         -- usuario_faixa_etaria_ordem,
         usuario_sexo_id_sigtap,
         sum(coalesce(ativos_mes, 0)) AS ativos_mes,
-        sum(coalesce(ativos_3meses, 0)) AS ativo_3meses,
+        sum(coalesce(ativos_3meses, 0)) AS ativos_3meses,
         sum(coalesce(tornandose_inativos, 0)) AS tornandose_inativos,
         now() AS atualizacao_data
     FROM com_combinacoes 
