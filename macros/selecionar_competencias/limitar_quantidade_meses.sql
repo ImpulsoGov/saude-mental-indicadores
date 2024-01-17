75b681ed751583af0e951d1881b7a84c243e14cc
fernandesbruna <brunadesfernandes@gmail.com>
2024-01-17 16:17:52 -0300

    feat: :sparkles: Atualiza macro de limitar competências para lidar com campo de periodo_id

--- macros/selecionar_competencias/limitar_quantidade_meses.sql
+++ macros/selecionar_competencias/limitar_quantidade_meses.sql
@@ -15,19 +15,46 @@ SPDX-License-Identifier: MIT
     )
 -%}
 
-ultimos_meses_municipio AS (
-    SELECT
-        {{ coluna_municipio_id }},
-        MAX({{ coluna_data }}) as max_data
-    FROM {{ relacao }}
-    GROUP BY {{ coluna_municipio_id }}
-),
+{% set necessario_tabela_periodos = coluna_data == "periodo_id" %}
+
+{% if necessario_tabela_periodos %}
+    ultimos_meses_municipio AS (
+        SELECT 
+            umm_sem_periodo_id.*,
+            periodos.id as periodo_id
+        FROM (
+            SELECT
+                {{ coluna_municipio_id }},
+                MAX(periodos.data_inicio) as max_data
+            FROM {{ relacao }} t
+            LEFT JOIN {{ source("codigos", "periodos") }} periodos ON t.periodo_id = periodos.id
+            GROUP BY {{ coluna_municipio_id }}
+        ) umm_sem_periodo_id
+        LEFT JOIN {{ source("codigos", "periodos") }} periodos ON umm_sem_periodo_id.max_data = periodos.data_inicio
+        ),
+{% else %}
+    ultimos_meses_municipio AS (
+        SELECT
+            {{ coluna_municipio_id }},
+            MAX({{ coluna_data }}) as max_data
+        FROM {{ relacao }} t
+        LEFT JOIN {{ source("codigos", "periodos") }} periodos ON t.periodo_id = periodos.id
+        GROUP BY {{ coluna_municipio_id }}
+    ),
+{% endif %}
+
 {{ cte_resultado }} AS (
     SELECT 
         t.*
     FROM {{ relacao }} t
     JOIN ultimos_meses_municipio ON t.{{ coluna_municipio_id }} = ultimos_meses_municipio.{{ coluna_municipio_id }}
-    WHERE t.{{ coluna_data }} >= (ultimos_meses_municipio.max_data - INTERVAL '{{ quantidade_meses_a_limitar }} months'::interval) 
+    {% if necessario_tabela_periodos %}
+        LEFT JOIN {{ source("codigos", "periodos") }} periodos ON t.periodo_id = periodos.id
+        WHERE periodos.data_inicio >= (ultimos_meses_municipio.max_data - INTERVAL '{{ quantidade_meses_a_limitar }} months'::interval)     
+    {% else %}
+        WHERE t.{{ coluna_data }} >= (ultimos_meses_municipio.max_data - INTERVAL '{{ quantidade_meses_a_limitar }} months'::interval) 
+    {% endif %}    
 )
 
 {% endmacro %}
+
