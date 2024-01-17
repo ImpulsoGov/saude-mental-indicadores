ed2d7f88d6714949746ba6887a641f198901d6a1
fernandesbruna <brunadesfernandes@gmail.com>
2024-01-17 16:18:31 -0300

    feat: :sparkles: Atualiza macro de selecionar municípios ativos para lidar com campo de unidade_geografica_id

--- macros/utilidades/selecionar_municipios_ativos.sql
+++ macros/utilidades/selecionar_municipios_ativos.sql
@@ -13,6 +13,8 @@ SPDX-License-Identifier: MIT
     )
 -%}
 
+{% set usar_municipio_id = coluna_municipio_id == "unidade_geografica_id" %}
+
 municipios_ativos AS (
 	SELECT * FROM {{ source("utilidades", "municipios_painel_sm") }}
     WHERE status_ativo = TRUE
@@ -22,6 +24,12 @@ municipios_ativos AS (
         tabela.*
     FROM {{ relacao }} tabela
     INNER JOIN municipios_ativos 
-    ON tabela.{{ coluna_municipio_id }} = municipios_ativos.id_sus
+
+    {% if usar_municipio_id %}
+        ON tabela.{{ coluna_municipio_id }} = municipios_ativos.id_mun
+    {% else %}
+        ON tabela.{{ coluna_municipio_id }} = municipios_ativos.id_sus
+    {% endif %}  
 )
+
 {% endmacro %}
